d73a055af1a2cebf59aff2de1ae9aa44abb4453c
shreyaapradhan <shreyapradhan0706@gmail.com>
2026-01-18 08:44:11 +0530

    delete

--- hackathon1/demo/src/main/java/com/hackathon/demo/repository/DangerZoneRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hackathon.demo.repository;
-
-import com.hackathon.demo.model.DangerZone;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DangerZoneRepository extends JpaRepository<DangerZone, Long> {
-}
